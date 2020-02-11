abc0dfe85267ed4b146b4fd284ebfe1c431e0fd5
Benjamin Bakle <bbakle@apterainc.com>
2020-02-10 19:06:00 -0500

    fix: add captital Login.js

---
+++ src/authentication/Login.js
@@ -0,0 +1,17 @@
+import { useAuthentication } from "./authentication-context";
+import React from 'react';
+import Loading from "../components/Loading/Loading";
+
+export default function Login(props) {
+    const { login, refreshContext } = useAuthentication();
+
+    const _login = () => {
+        login && login();
+    }
+    return (
+        <>
+            {_login()}
+            <Loading />
+        </>
+    )
+}
--- src/authentication/Logout.js
+++ src/authentication/Logout.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAuthentication } from "./authentication-context";
 import Loading from "../components/Loading/Loading";
 
